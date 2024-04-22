a3cf8c0a6324480d812e227ad5d93712515e3004
Udenkov Vitaly <VUdenkov@egartech.ru>
2024-04-22 19:49:08 +0300

    submodule added

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -1,10 +1,16 @@
-﻿namespace ConsoleApp3
+﻿using ClassLibrary1;
+
+namespace ConsoleApp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
+
+            var lib1 = new Lib1();
+            lib1.Run();
         }
     }
 }
